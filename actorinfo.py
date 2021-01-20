169ea864481d7048bb431f7c31e989ea230406ad
keenan-h <keenanhurley@gmail.com>
2021-01-19 20:55:52 -0500

    add first template

---
+++ actorinfo.py
@@ -0,0 +1,12 @@
+from flask import Markup
+
+# fill in each between quotation marks like "this" or like '''this'''
+# if you do not have one of the social media accounts and don't want it to show. Write None with no quotation marks.
+
+full_name = "Ian Edlund"
+bio = ["Ian Edlund is a Brooklyn based actor for theater and film.", Markup('''He co-wrote the screenplay and starred in the short <a href="https://vimeo.com/265361498" target="_blank">Mixtape Marauders</a>, which screened at the Clermont-Ferrand International Short Film Festival and was nominated for a Vimeo Award for Best Comedy of the Year. He also recently acted in the short <a href="https://vimeo.com/361425855" target="_blank">The Claw</a> and in the music video for Erik Blood's song <a href="https://vimeo.com/149197991" target="_blank">Chase The Clouds</a>.'''), "He recieved his BA in Theater from Fordham University where he studied with Stephen McKinley Henderson, Tina Benko, and Daniel Alexander Jones."]
+reel = "https://vimeo.com/501874931"
+twitter = "https://twitter.com/ianedlund"
+instagram = None
+facebook = None
+imdb = "https://www.imdb.com/name/nm6656980/"
--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from actorinfo import full_name, bio, reel, twitter, instagram, facebook, imdb
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', full_name=full_name, bio=bio, reel=reel, twitter=twitter, instagram=instagram, facebook=facebook, imdb=imdb)
 
 
 if __name__ == '__main__':
